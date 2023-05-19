2ee161e9703599177e6d75da53d891afb1c7ce26
Samayun Chowdhury <samayun.m.chowdhury@gmail.com>
2023-05-19 22:20:13 +0600

    feat(LinerSearch): implement

---
+++ LinearSearch.cpp
@@ -0,0 +1,39 @@
+/*
+* Author: Samayun Chowdhury
+* Source Code: https://github.com/samayun/data-structures-and-algorithms/blob/master/LinearSearch.cpp
+*/
+
+#include <iostream>
+
+using namespace std;
+
+/*
+* int arr[] is the linear array
+* size of LA
+* target elment is the third parameter
+* @return index
+*/
+int linearSearch(const int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i; 
+        }
+    }
+    return -1;
+}
+
+int main() {
+    const int size = 5;
+    int arr[size] = {10, 20, 30, 40, 50};
+    int target = 30;
+
+    int result = linearSearch(arr, size, target);
+
+    if (result != -1) {
+        cout << "Element " << target << " found at index " << result << endl;
+    } else {
+        cout << "Element " << target << " not found in the array." << endl;
+    }
+
+    return 0;
+}
